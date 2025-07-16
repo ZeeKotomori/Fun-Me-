4df0ebb630357e5a68a1c6ffcedee258954af5cf
ZeeKotomori <zikriaf23@gmail.com>
2025-07-16 14:19:43 +0700

    feat: update About page content for clarity and engagement

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Navbar from '../components/Navbar';
 import PageWrapper from '../components/PageWrapper';
 
@@ -10,22 +9,23 @@ export default function About() {
                 <h1 className="text-2xl font-bold mb-2">About App</h1>
                 <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                     <li className='tracking-[-.01em]'>
-                        <span className="font-semibold">Fun Me!</span> created as an assignment from the Jabar Digital Academy program.
+                        <span className="font-semibold">Fun Me!</span> is your daily source of quirky knowledge and meaningful moments.
                     </li>
                     <li className='tracking-[-.01em]'>
-                        This app displays Fun facts of the day, <span className='font-semibold'>Every Day</span>. 
+                        This app shows Fun facts of the day, <span className='font-semibold'>Every Day</span>.
                     </li>
                     <li className='tracking-[-.01em]'>
-                        You can see the facts of the day by clicking the button below or by visiting the <Link href="/fun-fact/today" className="text-blue-500 hover:underline">Fun Fact Today</Link> page.
+                        Not just facts <span className='font-semibold'>Now</span> you can write your own notes too!
                     </li>
                     <li className='tracking-[-.01em]'>
-                        or you can type /fun-fact/today or fun-fact/[number] after domain
+                        Send messages, lock them with a key 🔐, and even attach music 🎵
                     </li>
                     <li className='tracking-[-.01em]'>
-                        Made using technology <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">Next.js 15</code>, <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">Tailwind CSS</code>, dan <code className='"bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold"'>React</code>.
+                        Make them private, and share to your crush 💖
                     </li>
+
                     <li className='tracking-[-.01em]'>
-                        #new_day_new_facts
+                        #see_fact_and_leave_note
                     </li>
                 </ol>
             </PageWrapper>
