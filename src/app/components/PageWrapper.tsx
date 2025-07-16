cc209d3d15758b9cb7fd06df99e22c2b1ae14f97
ZeeKotomori <zikriaf23@gmail.com>
2025-07-16 14:46:31 +0700

    feat: add background color to PageWrapper for improved visual consistency

--- src/app/components/PageWrapper.tsx
+++ src/app/components/PageWrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function PageWrapper({ children }: { children: ReactNode }) {
     return (
-        <main className="flex-grow flex flex-col items-center justify-center px-4 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex-grow flex flex-col bg-[#0a0a0a] items-center justify-center px-4 font-[family-name:var(--font-geist-sans)]">
             {children}
         </main>
     );
